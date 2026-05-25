f5dd9294d1280d40e2fd6a02845eba5505ec7932
wadhwaniprakriti962-cmyk <wadhwaniprakriti962@gmail.com>
2026-05-25 12:17:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
-const dotenv = require("dotenv");
+
 const authRoutes = require("./routes/authRoutes");
-
-const connectDB = require("./config/db");
 const internRoutes = require("./routes/internRoutes");
-
-dotenv.config();
+const connectDB = require("./config/db");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // middleware
 app.use(express.json());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 
 // routes
 app.use("/api/interns", internRoutes);
@@ -27,8 +29,12 @@ app.get("/", (req, res) => {
 });
 
 // start server AFTER DB connects
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("DB Connection Error:", err);
   });
-});
